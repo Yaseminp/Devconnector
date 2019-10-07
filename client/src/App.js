01eabc6c1b7e0847adc9b2a5066edc20996318c0
Yaseminp <53197333+Yaseminp@users.noreply.github.com>
2019-10-06 19:14:37 -0700

    add html

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import{BrowserRouter as Router, Route} from 'react-router-dom';
+import Register from './component/auth/Register';
+import Login from './component/auth/Login';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-     <h1>My React app</h1> 
+      <Navbar/>
+      <Route exact path="/" component={Landing} />
+      {Landing} />
+      <div className="container">
+        <Route exact path = "/register" component ={Register} />
+        <Route exact path ="/Login" component = {Login} />
+      </div>
+     <Footer/>
     </div>
+    </Router>
   );
 }
 
